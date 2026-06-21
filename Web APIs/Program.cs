cc2b11f934864bc0b65645f951d3bd33d803eb22
25TW1059 <25TW1059@barton.ac.uk>
2026-06-21 15:34:11 +0100

    Test with xkcd fetching

--- Web APIs/Program.cs
+++ Web APIs/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Web_APIs
 {
@@ -10,6 +14,27 @@ namespace Web_APIs
     {
         static void Main(string[] args)
         {
+            WebRequest request = WebRequest.Create("https://xkcd.com/info.0.json");
+            request.Method = WebRequestMethods.Http.Get;
+            request.ContentType = "application/json; charset=utf-8;";
+
+            string text;
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+            {
+                text = sr.ReadToEnd();
+            }
+
+            Console.WriteLine(text + "\n\n");
+
+            Comic comic = JsonSerializer.Deserialize<Comic>(text);
+
+
+            Console.WriteLine($"Title: {comic.title}, Published: {comic.day}-{comic.month}-{comic.year}\n{comic.alt}");
+
+            Console.ReadKey();
         }
     }
 }
